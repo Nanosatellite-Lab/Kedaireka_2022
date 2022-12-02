0871e828fa2e613efc8b8eb02af91b0d43658f19
kanyazz <115144640+kanyazz@users.noreply.github.com>
2022-12-02 10:53:15 +0700

    Change Code

--- TTC/Arduino/TTC_RX/TTC_RX.ino
+++ TTC/Arduino/TTC_RX/TTC_RX.ino
@@ -1,135 +1,46 @@
-#include <Si446x.h> // include Si446x.h library
+#include <RH_RF24.h>  // include Si446x.h library
 
-#define Channel 127 // Channel (0 - 255)
-#define Max_Packet_Size 128 // Maximum packet size @128
+#define Max_Packet_Size 128  // Maximum packet size @128
+#define rf_receive A2
+#define rf_transmit A3
 
-#define Packet_None 0 // State for packet if packet contain nothing
-#define Packet_Ok 1 // State for packet if packet contain something
-#define Packet_Invalid 2 // State for packet if packet is invalid (transmitter not working, etc)
+uint8_t buf[Max_Packet_Size];
+uint8_t len = sizeof(buf);
+static uint8_t counter;   // Variable to count number of received data
+static uint8_t invalids;  // Variable to count number of failed received data
+static uint32_t tries;  // Variable to count number of tries received data
 
-// Custom data type for packet handling
-typedef struct{
-	uint8_t ready;
-	int16_t rssi;
-	uint8_t length;
-	uint8_t buffer[Max_Packet_Size];
-} packetReceived_t;
 
-static packetReceived_t packetReceived; // Init the received packet struct
-static uint32_t counter; // Variable to count number of transmitted data
-static uint32_t invalids; // Variable to count number of invalid transmitted data
-
-si446x_info_t info; // Buffer for storing module info
-si446x_info_t *info_ptr = &info; // Pointer for the info buffer
-
-// Callback when RX is complete
-void SI446X_CB_RXCOMPLETE(uint8_t length, int16_t rssi){
-	if(length > Max_Packet_Size)
-		length = Max_Packet_Size;
-
-	packetReceived.ready = Packet_Ok;
-	packetReceived.rssi = rssi;
-	packetReceived.length = length;
-
-	Si446x_read((uint8_t*)packetReceived.buffer, length);
-}
-
-// Callback when RX is Failed
-void SI446X_CB_RXINVALID(int16_t rssi){
-	packetReceived.ready = Packet_Invalid;
-	packetReceived.rssi = rssi;
-}
-
-// Function to get the module's info
-void ModuleInfo(){
-  Si446x_getInfo(info_ptr);
-
-  Serial.print("Chip Revision          : ");
-  Serial.println(info.chipRev);
-
-  Serial.print("Customer               : ");
-  Serial.println(info.customer);
-
-  Serial.print("Function               : ");
-  Serial.println(info.func);
-
-  Serial.print("Id                     : ");
-  Serial.println(info.id);
-
-  Serial.print("Part Id                : ");
-  Serial.println(info.part);
-
-  Serial.print("Part Build             : ");
-  Serial.println(info.partBuild);
-
-  Serial.print("Patch                  : ");
-  Serial.println(info.patch);
-
-  Serial.print("Revision Branch        : ");
-  Serial.println(info.revBranch);
-
-  Serial.print("Revision External      : ");
-  Serial.println(info.revExternal);
-
-  Serial.print("Revision Internal      : ");
-  Serial.println(info.revInternal);
-
-  Serial.print("ROM ID                 : ");
-  Serial.println(info.romId);
-
-  Serial.print("Temperature            : ");
-  Serial.println(Si446x_adc_temperature());
-
-  Serial.print("RF4463 State           : ");
-  Serial.println(Si446x_getState());
-
-  Serial.println("+++++++++++++++++++++++++");
-  Serial.println();
-}
+RH_RF24 rf4463(A4, A0, A1);  // Initialize RF4463 Object
 
 // Main Setup function
 void setup() {
-  Serial.begin(115200);
-  Si446x_init();
+  pinMode(rf_receive, OUTPUT);
+  pinMode(rf_transmit, OUTPUT);
 
-  ModuleInfo();
+  digitalWrite(rf_receive, HIGH);
+  digitalWrite(rf_transmit, LOW);
+
+  Serial.begin(115200);
+
+  if (!rf4463.init())
+    Serial.println("Init Failed");
+  Serial.println("Success");
 }
 
 // Main Loop function
 void loop() {
-  Si446x_RX(Channel);
-
-  Serial.println("Waiting for Packet...");
-  while(packetReceived.ready == Packet_None);
-
-  if(packetReceived.ready != Packet_Ok)
-	{
-		invalids++;
-		packetReceived.ready = Packet_None;
-		Serial.print("Invalid packet! Signal: ");
-		Serial.print(packetReceived.rssi);
-		Serial.println("dBm");
-
-		Si446x_RX(Channel);
-	}else{
+  if (rf4463.recv(buf, &len)) {
+    Serial.print("Tries : ");
+    Serial.print(tries);
+    Serial.print(" --> Received Message: ");
+    Serial.println((char*)buf);
     counter++;
-    packetReceived.ready = Packet_None;
-
-    Serial.println("Got packet, Receiving data...");
-
-    Serial.print("Signal strength: ");
-		Serial.print(packetReceived.rssi);
-		Serial.println("dBm");
-    Serial.print("Data from Transmitter: ");
-		Serial.write((uint8_t*)packetReceived.buffer, sizeof(packetReceived.buffer));
-		Serial.println();
+  } else {
+    Serial.print("Tries : ");
+    Serial.print(tries);
+    Serial.println(" --> recv failed");
+    invalids++;
   }
-
-  Serial.print("Totals: ");
-	Serial.print(counter);
-	Serial.print("Data, ");
-	Serial.print(invalids);
-	Serial.println("Invalid");
-	Serial.println("+++++++");
-  delay(1000);
+  tries++;
 }
--- TTC/Arduino/TTC_TX/TTC_TX.ino
+++ TTC/Arduino/TTC_TX/TTC_TX.ino
@@ -1,6 +1,9 @@
 #include <RH_RF24.h> // include Si446x.h library
 
 #define Max_Packet_Size 128
+#define rf_receive A2
+#define rf_transmit A3
+// #define power_hpa B8
 
 char message[Max_Packet_Size] = "LOREM IPSUM DOLOR SIT AMET"; // Test Message
 char *message_ptr = &message[Max_Packet_Size]; // Pointer for variable "message"
@@ -11,6 +14,14 @@ RH_RF24 rf4463(A4, A0, A1); // Initialize RF4463 Object
 
 // Main Setup function
 void setup() {
+  pinMode(rf_receive, OUTPUT);
+  pinMode(rf_transmit, OUTPUT);
+  //pinMode(power_hpa, OUTPUT);
+
+  digitalWrite(rf_receive, LOW);
+  digitalWrite(rf_transmit, HIGH);
+  //digitalWrite(power_hpa, HIGH);
+
   Serial.begin(115200);
 
   while (!rf4463.init())
